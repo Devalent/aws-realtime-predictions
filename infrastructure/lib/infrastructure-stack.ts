7a9df4ed6f7bb99dbd3734ecdfb59bd00d74b008
Artem Abashev <artem@abashev.com>
2022-02-11 11:18:42 +0300

    DAGs deployment.

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -8,8 +8,36 @@ export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const bucketDag = new cdk.aws_s3.Bucket(this, 'bucket-dag', {
+      bucketName: `${name}-dag`,
+    });
+
     const bucketData = new cdk.aws_s3.Bucket(this, 'bucket-data', {
       bucketName: `${name}-data`,
+    });
+
+    const roleGlue = new cdk.aws_iam.Role(this, 'role-glue', {
+      roleName: `${name}-glue`,
+      assumedBy: new cdk.aws_iam.ServicePrincipal('glue.amazonaws.com'),
+      managedPolicies: [
+        { managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole' },
+      ],
+      inlinePolicies: {
+        'inline-policy-1': new cdk.aws_iam.PolicyDocument({
+          statements: [
+            new cdk.aws_iam.PolicyStatement({
+              effect: cdk.aws_iam.Effect.ALLOW,
+              actions: [
+                's3:*',
+              ],
+              resources: [
+                `${bucketDag.bucketArn}*`,
+                `${bucketData.bucketArn}*`,
+              ],
+            }),
+          ],
+        }),
+      },
     });
   }
 }
