a7d306580b7ef8757ea98bf56d800ff275307b76
Artem Abashev <artem@abashev.com>
2022-02-20 14:21:32 +0300

    Stack update.

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -4,6 +4,10 @@ import * as cdk from 'aws-cdk-lib';
 
 const name = 'realtime-predictions';
 
+const tags = {
+  stack: name,
+};
+
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -14,7 +18,12 @@ export class InfrastructureStack extends Stack {
 
     const bucketData = new cdk.aws_s3.Bucket(this, 'bucket-data', {
       bucketName: `${name}-data`,
+
     });
+
+    // bucketData.addObjectCreatedNotification({
+      
+    // });
 
     const roleGlue = new cdk.aws_iam.Role(this, 'role-glue', {
       roleName: `${name}-glue`,
@@ -39,5 +48,69 @@ export class InfrastructureStack extends Stack {
         }),
       },
     });
+
+    const jobEtl = new cdk.aws_glue.CfnJob(this, 'job-etl', {
+      tags,
+      name: `${name}-etl`,
+      description: 'Prepare conversions data for model training.',
+      role: roleGlue.roleArn,
+      command: {
+        name: 'glueetl',
+        pythonVersion: '3',
+        scriptLocation: `s3://${bucketDag.bucketName}/conversions_etl.py`,
+      },
+      glueVersion: '3.0',
+      workerType: 'Standard',
+      numberOfWorkers: 2,
+    });
+
+    const rolePipeline = new cdk.aws_iam.Role(this, 'role-pipeline', {
+      roleName: `${name}-pipeline`,
+      assumedBy: new cdk.aws_iam.ServicePrincipal('states.amazonaws.com'),
+      inlinePolicies: {
+        'inline-policy-1': new cdk.aws_iam.PolicyDocument({
+          statements: [
+            new cdk.aws_iam.PolicyStatement({
+              effect: cdk.aws_iam.Effect.ALLOW,
+              actions: [
+                'databrew:StartJobRun',
+                'databrew:ListJobRuns',
+              ],
+              resources: [
+                `*`,
+              ],
+            }),
+          ],
+        }),
+      },
+    });
+
+    // new cdk.aws_databrew.
+
+    new cdk.aws_stepfunctions.CfnStateMachine(this, 'state', {
+      stateMachineName: `${name}-pipeline`,
+      roleArn: rolePipeline.roleArn,
+      definition: {
+        "Comment": "Load, filter, prepare, train and deploy.",
+        "StartAt": "Brew",
+        "States": {
+          "Brew": {
+            "Next": "Pass",
+            "Parameters": {
+              "Name": "test"
+            },
+            "Resource": "arn:aws:states:::databrew:startJobRun.sync",
+            "Type": "Task"
+          },
+          "Pass": {
+            "Comment": "A Pass state",
+            "End": true,
+            "Type": "Pass"
+          }
+        }
+      },
+    });
+
+    new cdk.aws_glue.
   }
 }
